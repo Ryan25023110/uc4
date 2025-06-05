dd736e560d8fd6b81a4743b21f763ebe050a8c58
Ryan25023110 <ryanaraujo2502@gmail.com>
2025-06-05 17:10:30 -0300

    Add files via upload

---
+++ aula/aula.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+    int idade = 25;
+    float altura = 1.75;
+    char letra = 'A';
+
+    printf("idade: %d\n", idade);
+    printf("altura: %.2f\n", altura);
+    printf("Letra: %c\n", letra);
+
+    return 0;
+
+}
